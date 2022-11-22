eba3646e219d54459e41dac1cc2d4266949df53e
Bill T <billtasabia@gmail.com>
2022-11-22 15:55:31 -0800

    updated Input.cpp

--- code/BassCat.cpp
+++ code/BassCat.cpp
@@ -1,1 +1,11 @@
 //This will be the final main file
+#include "Engine.h"
+
+int main()
+{
+    Engine engine;
+
+    engine.run();
+
+    return 0;
+}
---
+++ code/Input.cpp
@@ -0,0 +1,32 @@
+#include "Engine.h"
+
+void Engine::input()
+{
+    Event event;
+    while(m_Window.pollEvent(event));
+    {
+        if (event.type == Event::KeyPressed)
+        {
+            // Quit game
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
+            {
+                m_Window.close();
+            }
+            //start game / unpause
+            if (Keyboard::isKeyPressed(Keyboard::Return))
+			{
+				m_Playing = true;
+			}
+        }
+
+    }
+    //Example from CH15
+    //Not sure why this works outside the while loop?
+    /*
+    //Handle input specific to Thomas
+	if (m_Thomas.handleInput())
+	{
+		// Play a jump sound
+	}
+    */
+}
