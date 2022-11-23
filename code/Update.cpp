1084e1f9ef00028c2041b8a707435c404d122731
Bill T <billtasabia@gmail.com>
2022-11-22 16:00:46 -0800

    updated Update.cpp

---
+++ code/Update.cpp
@@ -0,0 +1,31 @@
+#include "Engine.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+void Engine::update(float dtAsSeconds)
+{
+    if (m_Playing)
+	{
+        /* //Example CH15
+		// Update Thomas
+		m_Thomas.update(dtAsSeconds);
+
+		// Update Bob
+		m_Bob.update(dtAsSeconds);
+
+		// Count down the time the player has left
+		m_TimeRemaining -= dtAsSeconds;
+
+		// Have Thomas and Bob run out of time?
+		if (m_TimeRemaining <= 0)
+		{
+			m_NewLevelRequired = true;
+		}
+        */
+	}// End if playing
+
+    //update views, other objects depending on the game state
+
+
+}
