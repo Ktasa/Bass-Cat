3b04bff107a4f42db2b885c1045e4218c4ac7962
Bill T <billtasabia@gmail.com>
2022-11-24 20:32:51 -0800

    updated header

--- code/Character.h
+++ code/Character.h
@@ -9,13 +9,14 @@ using namespace std;
 
 enum CharacterType{CAT, DOG};
 enum Side{LEFT, RIGHT};
+enum LineColor{BLACK, WHITE};
 
 const int CHARACTER_SHEET_WIDTH = 1000;
 
 class Character
 {
 public:
-    Character(CharacterType type);
+    Character(CharacterType type, LineColor color, Side side);
     //Sprite getSprite(); //unnecessary? (pass texture to draw by reference)
     void updateCharacter(float dtAsSeconds);
 
@@ -24,20 +25,37 @@ public:
     void setFrameTime(float seconds);
 
 private:
-    Texture m_spritesheetTexture;
+    Texture m_charTexture;
     VertexArray charVA; //holds coords for sprites in sheet
     //Sprite m_sprite; //unnecessary? (pass texture to draw by reference)
         
-    CharacterType m_type; //cat or dog
+    //CharacterType m_type; //cat or dog
+    //LineColor m_lineColor;
     Side m_side; //left or right
 
     Vector2f position; //screen position
 
-    int m_frameNum; //current frame
+    int m_frameID; //current frame
     float m_timePerFrame; //time per frame
     float m_timeUntilUpdate; 
 
     bool isPressed; //is player input live
 };
+
+Character::Character(CharacterType type, LineColor color, Side side)
+{
+    //load texture file
+    //CHANGE TO USE TEXTUREHOLDER
+    string fileName = "/graphics/spritesheet ";
+    if(type == CAT) {fileName += "cat ";}
+    else {fileName += "dog ";}
+    if(color == BLACK) {fileName += "B";}
+    else {fileName += "W";}
+    fileName += ".png";
+    m_charTexture.loadFromFile(fileName);
+
+    m_side = side;
+
+}
 
 #endif
