ac0da3c152ef6d18c4ed3c3616fa06e20e4d7b43
Bill T <billtasabia@gmail.com>
2022-11-26 19:25:00 -0800

    added color shifting

--- code/catAnimateExample.cpp
+++ code/catAnimateExample.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "HSV.h"
 
 using namespace sf;
 using namespace std;
@@ -37,8 +38,15 @@ int main()
     //Character.setPrimitiveType(Quads);
     //Character.resize(4);
 
+    //use "0, 100, 255" formula to make nice looking colors for Aura
+    Color blue = Color(0,100,255);
+    Color red = Color(255,0,100);
+    Color green = Color(0,255,100);
+    Color purple = Color(100,0,255);
+    Color orange = Color(255,100,0);
+
     const int CHARACTER_SHEET_WIDTH = 1000;
-    const float FRAME_TIME_S = 0.2f;
+    const float FRAME_TIME_S = 0.15f;
 
     Vector2f player1Position = {resolution.x * float(0.1), resolution.y * float(0.8)};
     Vector2f player2Position = {resolution.x * float(0.6), resolution.y * float(0.8)};
@@ -63,7 +71,7 @@ int main()
     auraSprite.setTexture(auraTexture);
     auraSprite.setScale(Vector2f{auraFlip,auraScale});
     auraSprite.setPosition(player1Position);
-    auraSprite.setColor(Color(0,100,255));
+    auraSprite.setColor(blue);
     auraSprite.setOrigin(AURA_SHEET_WIDTH/2,AURA_SHEET_WIDTH*float(0.6));
 
     Sprite c2Sprite;
@@ -78,16 +86,23 @@ int main()
     a2Sprite.setTexture(auraTexture);
     a2Sprite.setScale(Vector2f{auraFlip,auraScale});
     a2Sprite.setPosition(player2Position);
-    a2Sprite.setColor(Color(255,0,100));
+    a2Sprite.setColor(red);
     a2Sprite.setOrigin(AURA_SHEET_WIDTH/2,AURA_SHEET_WIDTH*float(0.6));
 
+    characterSprite.setColor(Color::White);
+    c2Sprite.setColor(Color::White);
+    auraSprite.setColor(blue);
+    a2Sprite.setColor(red);
 
     //Vector2f characterPosition = {0,0};
-
     //Character[0].position = characterPosition + Vector2f(0, 0);
     //Character[1].position = characterPosition + Vector2f(CHARACTER_SHEET_WIDTH, 0);
     //Character[2].position = characterPosition + Vector2f(CHARACTER_SHEET_WIDTH, CHARACTER_SHEET_WIDTH);
     //Character[3].position = characterPosition + Vector2f(0, CHARACTER_SHEET_WIDTH);
+
+    //use as base before shifting colors
+    Color color1 = blue;
+    Color color2 = red;
 
     Time character_animate_time;
     int characterFrame = 0;
@@ -115,11 +130,29 @@ int main()
             auraFlip *= -1;
             auraSprite.setScale(auraFlip,auraScale);
             a2Sprite.setScale(auraFlip, auraScale);
+
+            //if(characterFrame%2==0)
+            {
+                int hueDt = 10;
+                tuple<float,float,float> HSV = RGBtoHSV(color1);
+                float hue = get<0>(HSV);
+                hue += hueDt;
+                if(hue>360.0){hue=0.0;}
+                color1 = HSVtoRGB(hue,1,1);
+                auraSprite.setColor(color1);
+
+                HSV = RGBtoHSV(color2);
+                hue = get<0>(HSV);
+                hue += hueDt;
+                if(hue>360.0){hue=0.0;}
+                color2 = HSVtoRGB(hue,1,1);
+                a2Sprite.setColor(color2);
+            }
+
         }
 
         // Set the texture coordinates of each vertex
         int frameOffset = CHARACTER_SHEET_WIDTH * characterFrame;
-
         //Character[0].texCoords = Vector2f(0 + frameOffset, 0);
         //Character[1].texCoords = Vector2f(CHARACTER_SHEET_WIDTH + frameOffset, 0);
         //Character[2].texCoords = Vector2f(CHARACTER_SHEET_WIDTH + frameOffset, CHARACTER_SHEET_WIDTH);
@@ -133,9 +166,9 @@ int main()
         //window.draw(nebulaSprite);
         //window.draw(Character, &characterTexture);
         window.draw(auraSprite);
-        window.draw(a2Sprite);
+        //window.draw(a2Sprite);
         window.draw(characterSprite);
-        window.draw(c2Sprite);
+        //window.draw(c2Sprite);
         window.display();
     }
 }
