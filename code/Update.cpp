e00c02ed52365c48550a7d4cf5c1971f226aa270
Bill T <billtasabia@gmail.com>
2022-12-04 12:21:41 -0800

    updated comments

--- code/Update.cpp
+++ code/Update.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 void Engine::update(float dtAsSeconds)
 {
-	
 	//cout << "before update\n";
 	//cout << boolalpha << menu->isActive() << m_Playing << endl;
 	if(menu->isActive())
@@ -40,7 +39,7 @@ void Engine::update(float dtAsSeconds)
 		//cout << "entering updates\n";
 		//currently updates characters, CombatMenu
 		battle->update(dtAsSeconds);
-		BattleState state = battle->getState();
+		//BattleState state = battle->getState();
 		//cout << state << endl;
 		//cout << "exiting updates\n";
 	}// End if playing
